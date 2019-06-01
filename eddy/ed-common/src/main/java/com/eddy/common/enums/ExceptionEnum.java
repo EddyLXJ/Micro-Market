06a84b8161328f4ff5e6fac75ae4b86378ac102c
eddy <eddy.lxj@gmail.com>
2019-05-31 20:54:06 -0400

    finish the upload service

--- eddy/ed-common/src/main/java/com/eddy/common/enums/ExceptionEnum.java
+++ eddy/ed-common/src/main/java/com/eddy/common/enums/ExceptionEnum.java
@@ -13,7 +13,9 @@ public enum ExceptionEnum {
     CATEGORY_NOT_FOUND(400, "Category not found!"),
     BRAND_NOT_FOUND(404, "Brand not found!"),
     BRAND_SAVE_ERROR(500, "Brand save error"),
-    CATEGORY_BRAND_SAVE_ERROR(500, "Brand Category save error")
+    CATEGORY_BRAND_SAVE_ERROR(500, "Brand Category save error"),
+    UPLOAD_ERROR(500, "Upload error"),
+    INVALID_FILE_TYPE(400, "Invalid file type")
     ;
     private int code;
     private String msg;
--- eddy/ed-upload/src/main/java/com/eddy/upload/service/UploadService.java
+++ eddy/ed-upload/src/main/java/com/eddy/upload/service/UploadService.java
@@ -1,18 +1,61 @@
 package com.eddy.upload.service;
 
+import com.eddy.common.enums.ExceptionEnum;
+import com.eddy.common.exception.EdException;
+import com.eddy.upload.config.UploadProperties;
+import com.github.tobato.fastdfs.domain.StorePath;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @Service
+@Slf4j
+@EnableConfigurationProperties(UploadProperties.class)
 public class UploadService {
 
-    public String uploadImage(MultipartFile file){
-        //保存文件到本地
-        this.getClass().getClassLoader().getResource("").getFile()
-        File dest = new File();
-        //返回路径
-        return null;
+    @Autowired
+    private FastFileStorageClient storageClient;
+
+    @Autowired
+    private UploadProperties properties;
+    //private static final List<String> ALLOW_TYPE = Arrays.asList("image/jpeg", "image/png", "image/bmp", "image/jpg");
+
+    public String uploadImage(MultipartFile file) {
+        try {
+            // 校验文件类型
+            String contentType = file.getContentType();
+            if (!properties.getAllowTypes().contains(contentType)){
+                throw new EdException(ExceptionEnum.INVALID_FILE_TYPE);
+            }
+            // 校验文件内容
+            BufferedImage image = ImageIO.read(file.getInputStream());
+            if (image == null){
+                throw new EdException(ExceptionEnum.INVALID_FILE_TYPE);
+            }
+            String extension = StringUtils.substringAfterLast(file.getOriginalFilename(),".");
+            //准备目标路径
+            StorePath storePath = storageClient.uploadFile(file.getInputStream(), file.getSize(), extension, null);
+            //  保存文件到本地
+
+            // 返回路径
+            return properties.getBaseUrl() + storePath.getFullPath();
+        } catch (IOException e) {
+            // 上传失败
+            log.error("Upload File Error", e);
+            throw new EdException(ExceptionEnum.UPLOAD_ERROR);
+        }
+
     }
 }
